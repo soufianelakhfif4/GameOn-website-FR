87292a199fa73f6062fd7d4bf95f9594aedcf12a
Soufiane Lakhfif <soufiane.lakhfif@gmail.com>
2022-01-21 17:55:39 +0100

    Installation du projet + correction de la première issue

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,22 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtnClose = document.querySelectorAll(".modal-btn-close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
