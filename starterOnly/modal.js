7c5d960a7c47961bcf4d326254fb019674bd3636
Soufiane Lakhfif <soufiane.lakhfif@gmail.com>
2022-01-28 15:51:35 +0100

    Avancement sur le projet :     Ajout des valeurs for pour les labels     Ajout des vérifications + affichage des messages d'erreurs pour :         - le pénom         - le nom         - l'email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,3 +29,70 @@ function closeModal() {
 }
 
 
+// ########################################################################################
+// Validate input before submit
+// ########################################################################################
+const modalForm = document.getElementById("formReserve");
+modalForm.addEventListener("submit", validateDataOnSubmit);
+
+// Function to check if input form are validate
+function validateDataOnSubmit(event) {
+
+  // Get inputs
+  var first = document.getElementById("first");
+  var groupFirst = document.getElementById("groupFirst");
+  var last = document.getElementById("last");
+  var groupLast = document.getElementById("groupLast");
+  var email = document.getElementById("email");
+  var groupEmail = document.getElementById("groupEmail");
+
+  // Stop propagation before check inputs
+  event.preventDefault();
+
+  // Boolean to know if form can be submit
+  var error = false;
+
+  // Check if first name have least 2 digits
+  if(first.value.length < 2)
+  {
+    groupFirst.setAttribute('data-error-visible', true);
+    error = true;
+  } else {
+    groupFirst.setAttribute('data-error-visible', false);
+  }
+
+  // Check if last name have least 2 digits
+  if(last.value.length < 2)
+  {
+    groupLast.setAttribute('data-error-visible', true);
+    error = true;
+  } else {
+    groupLast.setAttribute('data-error-visible', false);
+  }
+
+
+  // Check if email is valide
+  if(!validateEmail(email.value)){
+    groupEmail.setAttribute('data-error-visible', true);
+    error = true;
+  } else {
+    groupEmail.setAttribute('data-error-visible', false);
+  }
+
+  // if no error, send form
+  if(!error){
+    modalForm.submit();
+  }
+
+}
+
+// Function to check email validation
+function validateEmail(email)
+{
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+  {
+    return true;
+  }
+  return false;
+}
+
